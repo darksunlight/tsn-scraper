6855db1ebc4ad3865ccd07642bfa485180b2f0f9
darksunlight <darksunlight210@gmail.com>
2021-12-29 02:10:50 -0800

    udpates

--- scraper.js
+++ scraper.js
@@ -1,14 +1,20 @@
 const fs = require('fs');
 const fetch = require('node-fetch');
+let count = parseInt(fs.readFileSync('./count','utf-8'));
 
-const sites = fs.readFileSync('./sites', 'utf-8').split(',').slice(2536);
+const sites = fs.readFileSync('./sites', 'utf-8').split(',').slice(count);
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-  }
+}
 async function main() {
     for (const site of sites) {
         fetch(site).then(res => res.text()).then(data => fs.writeFileSync(`./pages/${decodeURI(site.slice(29)).replaceAll(/\//g, '---')}`, data, 'utf-8'))
         await sleep(60);
+        count++;
+        if (count % 10 == 0) {
+            fs.writeFileSync('./count',count+"",'utf-8');
+            await sleep(1500);
+        }
     }
 }
-main()
+main();
